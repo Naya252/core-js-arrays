2acf74f85ccb8fc1d56ca3ab36bfd2388b3a89e0
Naya252 <naya252naya@gmail.com>
2024-01-10 23:05:19 +0300

    feat: add solution to findAllOccurrences

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -85,8 +85,9 @@ function findElement(arr, value) {
  *    findAllOccurrences([ null, undefined, null ], null) => 2
  *    findAllOccurrences([ true, 0, 1, 'true' ], true) => 1
  */
-function findAllOccurrences(/* arr, item */) {
-  throw new Error('Not implemented');
+function findAllOccurrences(arr, item) {
+  const res = arr.filter((el) => el === item);
+  return res.length;
 }
 
 /**
