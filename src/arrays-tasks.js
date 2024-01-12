7d9f4a427140b1d98f48f40f9c917aae6b677d4c
Naya252 <naya252naya@gmail.com>
2024-01-12 18:50:00 +0300

    feat: add solution to findCommonElements

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -548,8 +548,9 @@ function getMaxItems(arr, n) {
  *    findCommonElements(['a', 'b', 'c'], ['b', 'c', 'd']) => [ 'b', 'c' ]
  *    findCommonElements([1, 2, 3], ['a', 'b', 'c']) => []
  */
-function findCommonElements(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function findCommonElements(arr1, arr2) {
+  const res = Array.from(new Set(arr1.concat(arr2)));
+  return res.filter((el) => arr1.includes(el) && arr2.includes(el));
 }
 
 /**
